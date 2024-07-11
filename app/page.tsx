6f767bb7a49c50b89bf91c14640029dc0ccf043f
akuma-code <akumapl86@gmail.com>
2024-07-11 23:36:41 +0300

    sync

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,18 @@
-import Image from "next/image";
-
-export default function Home() {
+export default async function Home() {
+    const tt = await test();
+    console.clear();
+    console.log(await tt.json());
     return <main>Main</main>;
+}
+
+async function test() {
+    const res = fetch("https://api.timeweb.cloud/api/v1/auth/api-keys", {
+        headers: {
+            "Content-Type": "application/json",
+            Authorization: `Bearer ${process.env.TIMEWEB_CLOUD_TOKEN}`,
+            Origin: "localhost:3000",
+        },
+    });
+
+    return res;
 }
